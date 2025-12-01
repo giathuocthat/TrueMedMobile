c71291519d1b5dd86fa473e05d16534e263364a2
LocTrinh <trinhbaloc@gmail.com>
2025-12-01 09:41:52 +0700

    Add source flutter template

---
+++ true_med/lib/src/presentation/core/application_state/logout_provider/logout_provider.dart
@@ -0,0 +1,31 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../core/di/dependency_injection.dart';
+import 'logout_state.dart';
+
+part 'logout_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class Logout extends _$Logout {
+  @override
+  LogoutState build() {
+    return const LogoutState();
+  }
+
+  Future<void> call() async {
+    state = const LogoutState(type: Status.loading);
+
+    // Intentional simulated delay to show loading indicator
+    await Future.delayed(const Duration(seconds: 1));
+
+    try {
+      await ref.read(logoutUseCaseProvider).call();
+      // Invalidate all repository providers to remove cached data
+      ref.read(resetRepositoryUseCaseProvider).call(ref);
+
+      state = const LogoutState(type: Status.success);
+    } catch (e) {
+      state = LogoutState(type: Status.error, error: e.toString());
+    }
+  }
+}
---
+++ true_med/lib/src/presentation/features/authentication/login/riverpod/login_state.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../core/base/status.dart';
+
+export 'package:true_med/src/presentation/core/base/status.dart';
+
+part 'login_state.freezed.dart';
+
+@freezed
+abstract class LoginState<T> with _$LoginState<T> {
+  const factory LoginState({
+    @Default(false) bool rememberMe,
+    @Default(Status.initial) Status status,
+    String? error,
+  }) = _LoginState<T>;
+
+  const LoginState._();
+}
