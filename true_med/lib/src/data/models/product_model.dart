38693440537e65ac55f4ef74c9e1deb80dbc299d
LocTrinh <trinhbaloc@gmail.com>
2025-12-06 08:16:49 +0700

    add model product

--- true_med/lib/src/data/models/product_model.dart
+++ true_med/lib/src/data/models/product_model.dart
@@ -9,9 +9,23 @@ class ProductResponseModel extends ProductResponseEntity
     with ProductResponseModelMappable {
   ProductResponseModel({
     required super.id,
+    required super.categoryId,
     required super.name,
-    required super.price,
-    required super.image,
+    required super.shortDescription,
+    required super.fullDescription,
+    required super.slug,
+    required super.thumbnailUrl,
+    required super.ingredients,
+    required super.usageInstructions,
+    super.storageInstructions,
+    required super.registrationNumber,
+    required super.isPrescriptionDrug,
+    required super.isActive,
+    required super.isFeatured,
+    required super.createdDate,
+    super.updatedDate,
+    super.drugEfficacy,
+    super.dosageInstructions,
   });
 
   static const fromJson = ProductResponseModelMapper.fromJson;
--- true_med/lib/src/domain/entities/product_entity.dart
+++ true_med/lib/src/domain/entities/product_entity.dart
@@ -5,13 +5,41 @@ class ProductRequestEntity extends ProductEntity {}
 class ProductResponseEntity extends ProductEntity {
   ProductResponseEntity({
     required this.id,
+    required this.categoryId,
     required this.name,
-    required this.price,
-    required this.image,
+    required this.shortDescription,
+    required this.fullDescription,
+    required this.slug,
+    required this.thumbnailUrl,
+    required this.ingredients,
+    required this.usageInstructions,
+    this.storageInstructions,
+    required this.registrationNumber,
+    required this.isPrescriptionDrug,
+    required this.isActive,
+    required this.isFeatured,
+    required this.createdDate,
+    this.updatedDate,
+    this.drugEfficacy,
+    this.dosageInstructions,
   });
 
   final int id;
+  final int categoryId;
   final String name;
-  final double price;
-  final String image;
+  final String shortDescription;
+  final String fullDescription;
+  final String slug;
+  final String thumbnailUrl;
+  final String ingredients;
+  final String usageInstructions;
+  final String? storageInstructions;
+  final String registrationNumber;
+  final bool isPrescriptionDrug;
+  final bool isActive;
+  final bool isFeatured;
+  final String createdDate;
+  final String? updatedDate;
+  final String? drugEfficacy;
+  final String? dosageInstructions;
 }
